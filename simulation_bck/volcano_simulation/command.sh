0d6df5e4ade72060e4de0b150dcd561bfa0b4be2
ishanuc <ishanu.chattopadhyay@gmail.com>
2026-03-13 13:21:13 -0400

    upd

--- simulation_bck/volcano_simulation/command.sh
+++
@@ -1,1 +0,0 @@
-./volcano_simulation.py   --out_dir ./   --seed 3   --m 20000   --n_case 500000   --n_ctrl 500000   --beta_mean 0.06   --beta_sd 0.03   --eps 0.8   --n_val 1000   --var_extra_scale 1.0
