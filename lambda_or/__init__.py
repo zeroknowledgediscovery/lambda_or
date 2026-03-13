8ecd9635e71ee542e4213d5790c8a611314aa386
ishanuc <ishanu.chattopadhyay@gmail.com>
2026-03-13 15:58:42 -0400

    1

--- lambda_or/__init__.py
+++ lambda_or/__init__.py
@@ -1,4 +1,17 @@
-from .lambda_or import lambda_or, pq_from_two_gates, LambdaORResult
-__all__ = ["lambda_or", "pq_from_two_gates", "LambdaORResult", "neglog10_p_from_z"]
+from .lambda_or import (
+    lambda_or,
+    pq_from_two_gates,
+    LambdaORResult,
+    neglog10_p_from_z
+)
+
+__all__ = [
+    "lambda_or",
+    "pq_from_two_gates",
+    "LambdaORResult",
+    "neglog10_p_from_z",
+]
+
+
 __version__ = "0.1.0"
 
